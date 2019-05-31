90aa2c56732831e477e2e2eb96a1c5b6023d4890
Robert van den Heuvel <robert_vd_heuvel@hotmail.com>
2019-05-31 09:02:40 +0200

    Robert: create user toegevoegd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CreateUserComponent } from './create-user/create-user.component';
+
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    CreateUserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/create-user/create-user.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'create-user',
+  templateUrl: './create-user.component.html',
+  styleUrls: ['./create-user.component.css']
+})
+export class CreateUserComponent implements OnInit {
+  private user:User;
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+    this.user = new User();
+  }
+
+  create(): void {
+    this.userService.create(this.user).subscribe(user => {
+      this.user = new User();
+    });
+  }
+
+}
