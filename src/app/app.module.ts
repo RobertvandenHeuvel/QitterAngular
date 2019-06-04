aa4217c016545786c9d6a7729e6fd768f0f6f355
Robert van den Heuvel <robert_vd_heuvel@hotmail.com>
2019-06-04 14:04:33 +0200

    create user page weg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { CreateUserComponent } from './create-user/create-user.component';
 
 import { FormsModule } from '@angular/forms';
-import { CreateUserPageComponent } from './create-user-page/create-user-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
@@ -17,7 +16,6 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     AppComponent,
     UsersComponent,
     CreateUserComponent,
-    CreateUserPageComponent,
     LoginPageComponent,
     PageNotFoundComponent
   ],
